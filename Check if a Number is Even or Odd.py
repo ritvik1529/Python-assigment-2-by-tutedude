602743159ebd17cb37f1eb6aa3dee31de266cd28
Ritvik Goyal <ritvikgoyal300@gmail.com>
2025-06-29 12:10:09 +0530

    Add files via upload

---
+++ Check if a Number is Even or Odd.py
@@ -0,0 +1,6 @@
+a=int(input('Enter a number:'\s))
+if a%2==0:
+    print('Give number is even')
+else:
+    print('Given number is odd')
+
---
+++ Sum of Integers from 1 to 50 Using a Loop.py
@@ -0,0 +1,4 @@
+t=0
+for i in range(1,51):
+    t=t+i
+print('Sum of Integers from 1 to 50 is: ',t)
